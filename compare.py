05ad5dc0a94d900f957f4e5e9a392a3eb138c4c1
DrPepper18 <122201748+DrPepper18@users.noreply.github.com>
2023-01-08 20:38:04 +0300

    Update compare.py

--- compare.py
+++ compare.py
@@ -7,53 +7,53 @@ parser.add_argument('scorearg', type=str)
 args = parser.parse_args()
 
 
-#Data
-filesize = 0.0
-plgcnt = 0.0
-word1 = ''
-word2 = ''
-text = ''
-j=0
-
-
 #Reading input.txt file
 inputfile = open(args.inputarg).readlines()
 scorefile = open(args.scorearg, 'w+')
-while inputfile[0][j] != ' ':
-    text+=inputfile[0][j]
-    j+=1
-file1 = open(text).read()
-text = ''
-j+=1
-while j < len(inputfile[0]):
-    if inputfile[0][j] == '\n':
-        break
-    else:
-        text+=inputfile[0][j]
+
+
+#Comparing files
+line = 0
+for line in range(len(inputfile)):
+    #Data
+    filesize = 0.0
+    plgcnt = 0.0
+    word1 = ''
+    word2 = ''
+    text = ''
+    j=0    
+    while inputfile[line][j] != ' ':
+        text+=inputfile[line][j]
         j+=1
-file2 = open(text).read()
-text = ''
-
-
-#Comparing given files
-for j in range(len(file1)):
-    word1 = word1+file1[j]
-    if file1[j] == ' ':
-        if file1[j-1] != ' ':
-            filesize = filesize + 1
-            for i in range(len(file2)):
-                word2 = word2+file2[i]
-                if file2[i] == ' ':
-                    if word1 == word2:
-                        plgcnt = plgcnt+1
-                        break
-                    word2 = ''
-        word1 = '' 
-        
-        
-#Writing results
-print 'Plgt level: ', plgcnt/filesize*100, '%'
-scorefile.write(str(plgcnt/filesize*100)+'%')
-
+    file1 = open(text).read()
+    text = ''
+    j+=1
+    while j < len(inputfile[line]):
+        if inputfile[line][j] == '\n':
+            break
+        else:
+            text+=inputfile[line][j]
+            j+=1
+    file2 = open(text).read()
+    text = ''
+    #Comparing given files
+    for j in range(len(file1)):
+        word1 = word1+file1[j]
+        if file1[j] == ' ':
+            if file1[j-1] != ' ':
+                filesize = filesize + 1
+                for i in range(len(file2)):
+                    word2 = word2+file2[i]
+                    if file2[i] == ' ':
+                        if word1 == word2:
+                            plgcnt = plgcnt+1
+                            break
+                        word2 = ''
+            word1 = ''     
+    #Writing results
+    print 'Plgt level: ', plgcnt/filesize*100, '%'
+    scorefile.write(str(plgcnt/filesize)+'\n')
+    
+#Finishing
 scorefile.close()
 print('Compare completed. Results has been saved.')
