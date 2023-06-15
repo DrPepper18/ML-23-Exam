0b0fe7bd69caea5cde91c5e7669dccfe0e487596
DrPepper18 <122201748+DrPepper18@users.noreply.github.com>
2023-06-15 15:35:23 +0300

    Update compare.py
    Now it's on Python 3.5. The code has been optimized

--- compare.py
+++ compare.py
@@ -5,55 +5,27 @@ parser = argprs.ArgumentParser()
 parser.add_argument('inputarg', type=str)
 parser.add_argument('scorearg', type=str)
 args = parser.parse_args()
-
-
 #Reading input.txt file
 inputfile = open(args.inputarg).readlines()
 scorefile = open(args.scorearg, 'w+')
-
-
 #Comparing files
-line = 0
 for line in range(len(inputfile)):
     #Data
-    filesize = 0.0
-    plgcnt = 0.0
-    word1 = ''
-    word2 = ''
-    text = ''
-    j=0    
-    while inputfile[line][j] != ' ':
-        text+=inputfile[line][j]
-        j+=1
-    file1 = open(text).read()
-    text = ''
-    j+=1
-    while j < len(inputfile[line]):
-        if inputfile[line][j] == '\n':
-            break
-        else:
-            text+=inputfile[line][j]
-            j+=1
-    file2 = open(text).read()
-    text = ''
+    filesize = plgcnt = 0.0 
+    task = inputfile[line].split()
+    file1 = open(task[0]).read().split()
+    file2 = open(task[1]).read().split()
+    filesize = len(file1)
     #Comparing given files
     for j in range(len(file1)):
-        word1 = word1+file1[j]
-        if file1[j] == ' ':
-            if file1[j-1] != ' ':
-                filesize = filesize + 1
-                for i in range(len(file2)):
-                    word2 = word2+file2[i]
-                    if file2[i] == ' ':
-                        if word1 == word2:
-                            plgcnt = plgcnt+1
-                            break
-                        word2 = ''
-            word1 = ''     
+        for i in range(len(file2)):
+            if file1[j] == file2[i]:
+                plgcnt+=1
+                break
     #Writing results
-    print 'Plgt level: ', plgcnt/filesize*100, '%'
-    scorefile.write(str(plgcnt/filesize)+'\n')
-    
+    print('Plgt level: ', str(plgcnt/filesize*100)[:5], '%')
+    scorefile.write(str(plgcnt/filesize)[:5]+'\n')
 #Finishing
 scorefile.close()
-print('Compare completed. Results has been saved.')
+print('Compare completed. Results has been saved. Press [Enter] to quit.')
+input()
